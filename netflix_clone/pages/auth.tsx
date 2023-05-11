f0e7cfa947437d27f6d98d5a58f9c1f376d25d73
IamLam163 <olamidealiyu@gmail.com>
2023-05-11 13:33:53 +0100

    landing page

---
+++ netflix_clone/pages/auth.tsx
@@ -0,0 +1,15 @@
+const Auth = () => {
+  return (
+    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed gb-cover">
+      <div className="bg-black w-full h-full lg:bg-opacity-50">
+        <nav className="px-12 py-5">
+          <img src="/">
+        </nav>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default Auth;
---
+++ netflix_clone/pages/test.tsx
@@ -0,0 +1,7 @@
+const MyPage = () => {
+  return (
+    <h1>First Project in Typescript! Let's Go</h1>
+  )
+}
+
+export default MyPage;
